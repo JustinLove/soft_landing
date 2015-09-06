077b304b02dc902f6d0d3a8d99c3ab4d869c0f33
Justin Love <git@JustinLove.name>
2015-09-06 14:47:12 -0500

    transformed

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,22 @@ module.exports = function(grunt) {
           'pa/effects/specs/default_commander_landing.pfx'
         ],
         process: function(spec) {
-          console.log(spec)
+          spec.emitters = spec.emitters.filter(function(emit) {
+            // white shell
+            return emit.spec.papa != '/pa/effects/fbx/particles/sphere_ico16seg.papa'
+          })
+          spec.emitters.forEach(function(emit) {
+            if (emit.spec.shader == 'meshParticle_clip_smoke_bend') {
+              // smoke shell
+              emit.alpha = 0.5
+            } else if (emit.spec.baseTexture == '/pa/effects/textures/particles/softSmoke.papa' && emit.type == 'Cylinder_Z') {
+              // large expanding dust
+              emit.alpha[0][1] = 0.1
+            } else if (emit.spec.shape == 'pointlight') {
+              // bright blusih glow
+              emit.alpha = 0.05
+            }
+          })
         }
       },
     }

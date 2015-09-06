e053fa2ebefcedffa59bde0b8d5ec7856d3097d0
Justin Love <git@JustinLove.name>
2015-09-06 00:26:37 -0500

    vanilla effect

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+var spec = require('./lib/spec')
+var prompt = require('prompt')
+prompt.start()
+
+var modPath = '../../server_mods/com.wondible.pa.soft_landing/'
+var stream = 'stable'
+var media = require('./lib/path').media(stream)
+
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    clean: ['pa'],
+    // copy files from PA, transform, and put into mod
+    proc: {
+      // form 1: just the relative path, media src is assumed
+      landing: {
+        targets: [
+          'pa/effects/specs/default_commander_landing.pfx'
+        ],
+        process: function(spec) {
+          console.log(spec)
+        }
+      },
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerMultiTask('proc', 'Process unit files into the mod', function() {
+    if (this.data.targets) {
+      var specs = spec.copyPairs(grunt, this.data.targets, media)
+      spec.copyUnitFiles(grunt, specs, this.data.process)
+    } else {
+      var specs = this.filesSrc.map(function(s) {return grunt.file.readJSON(media + s)})
+      var out = this.data.process.apply(this, specs)
+      grunt.file.write(this.data.dest, JSON.stringify(out, null, 2))
+    }
+  })
+
+  // Default task(s).
+  grunt.registerTask('default', ['proc']);
+
+};
+
---
+++ README.md
@@ -0,0 +1,12 @@
+# Soft Landing
+
+Less dust during commander landing, so you can see initial placements.
+
+## Development
+
+The generated project includes a `package.json` that lists the dependencies, but you'll need to run `npm install` to download them.
+
+### Available Tasks
+
+- proc:landing - Proc: read one or more files from PA and munge into one in the mod.
+- default: proc
